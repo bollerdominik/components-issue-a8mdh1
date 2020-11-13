045cf519d57d674369d25d3622afea416d23d2ee
MEs Lis <fansarchive@protonmail.com>
2020-11-13 15:38:39 +0100

    music

--- src/app/example-component.ts
+++ src/app/example-component.ts
@@ -1,29 +1,45 @@
-import {Component, OnInit} from '@angular/core';
-import { timer } from 'rxjs';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'example-component',
   templateUrl: 'example-component.html',
   styleUrls: ['example-component.css'],
 })
-export class ExampleComponent implements OnInit {
+export class ExampleComponent implements OnInit, AfterViewInit {
 
-    progress = 0;
+  @ViewChild('audioElement') audioElement: ElementRef<HTMLAudioElement>;
+
+  showProgress: boolean;
 
   ngOnInit(): void {
-    timer(0, 500)
-      .subscribe(() => {
-        this.progress += 0.5
-      });
   }
 
-    onProgressChanged(progress: number): void {
-      console.log(progress);
-      this.progress = progress;
+  ngAfterViewInit(): void {
+  }
+
+  getProgress(): number {
+    return this.getCurrentSeconds() / this.getTotalTime() * 100;
+  }
+
+  getCurrentSeconds(): number {
+    return this.audioElement.nativeElement.currentTime * 1000;
+  }
+
+  getTotalTime(): number {
+    return this.audioElement.nativeElement.duration * 1000;
+  }
+
+  onProgressChanged(value: number) {
+    this.audioElement.nativeElement.currentTime = value;
+  }
+
+  onPlayClicked() {
+    this.showProgress = true;
+    this.audioElement.nativeElement.play();
   }
 }
 
 
 /**  Copyright 2019 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license */
